b63169746305eb8e6d6dd0a3878307cc6f6659cb
THEVOID1431 <165205305+Bbeniz@users.noreply.github.com>
2026-06-13 13:21:05 +0100

    Update logsbot.js

--- scripts/events/logsbot.js
+++ scripts/events/logsbot.js
@@ -5,7 +5,7 @@ module.exports = {
 		name: "logsbot",
 		isBot: true,
 		version: "1.4",
-		author: "𝕿𝖍𝖊 𝖁𝖔𝖎𝖉 𝕶𝖚𝖓 クン",
+		credits: "The VOID KUN クン",
 		envConfig: {
 			allow: true
 		},
@@ -14,23 +14,20 @@ module.exports = {
 
 	langs: {
 		vi: {
-			title: "☯ 𝐋𝐎𝐆 𝐒𝐘𝐒𝐓𝐄𝐌 𝐌𝐀𝐃𝐀𝐑𝐀 ☯",
-			added: "\n🩸 Événement : le bot a été invoqué dans un nouveau groupe\n⚔️ Responsable : %1",
-			kicked: "\n❌ Événement : le bot a été expulsé du groupe\n⚔️ Responsable : %1",
-			footer: "\n━━━━━━━━━━━━━━\n👤 ID utilisateur : %1\n📛 Groupe : %2\n🆔 ID groupe : %3\n⏰ Temps : %4"
+			title: "◤━━━━━━━━━━━━━━━━━━━━◥\n 👁️‍🗨️ 𝗠𝗔𝗗𝗔𝗥𝗔 𝗢𝗧𝗦𝗨𝗧𝗦𝗨𝗞𝗜 👁️‍🗨️\n◣━━━━━━━━━━━━━━━━━━━━◢\n ┌──────────────────┐\n │ 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 : 𝐥𝐨𝐠𝐬𝐛𝐨𝐭\n └──────────────────┘\n  \n🩸 𝐞́𝐯𝐞́𝐧𝐞𝐦𝐞𝐧𝐭 : le bot a été invoqué dans un nouveau groupe\n⚔️ 𝐫𝐞𝐬𝐩𝐨𝐧𝐬𝐚𝐛𝐥𝐞 : %1",
+			kicked: "◤━━━━━━━━━━━━━━━━━━━━◥\n 👁️‍🗨️ 𝗠𝗔𝗗𝗔𝗥𝗔 𝗢𝗧𝗦𝗨𝗧𝗦𝗨𝗞𝗜 👁️‍🗨️\n◣━━━━━━━━━━━━━━━━━━━━◢\n ┌──────────────────┐\n │ 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 : 𝐥𝐨𝐠𝐬𝐛𝐨𝐭\n └──────────────────┘\n  \n❌ 𝐞́𝐯𝐞́𝐧𝐞𝐦𝐞𝐧𝐭 : le bot a été expulsé du groupe\n⚔️ 𝐫𝐞𝐬𝐩𝐨𝐧𝐬𝐚𝐛𝐥𝐞 : %1",
+			footer: "\n👤 𝐮𝐬𝐞𝐫 𝐢𝐝 : %1\n📛 𝐠𝐫𝐨𝐮𝐩𝐞 : %2\n🆔 𝐠𝐫𝐨𝐮𝐩𝐞 𝐢𝐝 : %3\n⏰ 𝐭𝐞𝐦𝐩𝐬 : %4\n\n◤━━━━━━━━━━━━━━━━━━━━◥\n 🔮 𝘛𝘴𝘶𝘬𝘶𝘺𝘰𝘮𝘪 𝘐𝘯𝘧𝘪𝘯𝘪 𝘢𝘤𝘵𝘪𝘧\n◣━━━━━━━━━━━━━━━━━━━━◢"
 		},
 
 		en: {
-			title: "☯ MADARA LOG SYSTEM ☯",
-			added: "\n🩸 Event: bot has been added to a new group\n⚔️ Added by: %1",
-			kicked: "\n❌ Event: bot has been removed from group\n⚔️ Removed by: %1",
-			footer: "\n━━━━━━━━━━━━━━\n👤 User ID: %1\n📛 Group: %2\n🆔 Group ID: %3\n⏰ Time: %4"
+			title: "◤━━━━━━━━━━━━━━━━━━━━◥\n 👁️‍🗨️ 𝗠𝗔𝗗𝗔𝗥𝗔 𝗢𝗧𝗦𝗨𝗧𝗦𝗨𝗞𝗜 👁️‍🗨️\n◣━━━━━━━━━━━━━━━━━━━━◢\n ┌──────────────────┐\n │ 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 : 𝐥𝐨𝐠𝐬𝐛𝐨𝐭\n └──────────────────┘\n  \n🩸 𝐞𝐯𝐞𝐧𝐭 : bot has been added to a new group\n⚔️ 𝐚𝐝𝐝𝐞𝐝 𝐛𝐲 : %1",
+			kicked: "◤━━━━━━━━━━━━━━━━━━━━◥\n 👁️‍🗨️ 𝗠𝗔𝗗𝗔𝗥𝗔 𝗢𝗧𝗦𝗨𝗧𝗦𝗨𝗞𝗜 👁️‍🗨️\n◣━━━━━━━━━━━━━━━━━━━━◢\n ┌──────────────────┐\n │ 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 : 𝐥𝐨𝐠𝐬𝐛𝐨𝐭\n └──────────────────┘\n  \n❌ 𝐞𝐯𝐞𝐧𝐭 : bot has been removed from group\n⚔️ 𝐫𝐞𝐦𝐨𝐯𝐞𝐝 𝐛𝐲 : %1",
+			footer: "\n👤 𝐮𝐬𝐞𝐫 𝐢𝐝 : %1\n📛 𝐠𝐫𝐨𝐮𝐩 : %2\n🆔 𝐠𝐫𝐨𝐮𝐩 𝐢𝐝 : %3\n⏰ 𝐭𝐢𝐦𝐞 : %4\n\n◤━━━━━━━━━━━━━━━━━━━━◥\n 🔮 𝘛𝘴𝘶𝘬𝘶𝘺𝘰𝘮𝘪 𝘐𝘯𝘧𝘪𝘯𝘪 𝘢𝘤𝘵𝘪𝘧\n◣━━━━━━━━━━━━━━━━━━━━◢"
 		}
 	},
 
 	onStart: async ({ usersData, threadsData, event, api, getLang }) => {
 
-		
 		if (
 			(event.logMessageType == "log:subscribe" &&
 				event.logMessageData.addedParticipants.some(item => item.userFbId == api.getCurrentUserID()))
@@ -38,7 +35,6 @@ module.exports = {
 				event.logMessageData.leftParticipantFbId == api.getCurrentUserID())
 		) return async function () {
 
-			let msg = getLang("title");
 			const { author, threadID } = event;
 
 			// ⚔️ ignorer si action faite par le bot lui-même
@@ -46,6 +42,7 @@ module.exports = {
 				return;
 
 			let threadName;
+			let msg = "";
 			const { config } = global.GoatBot;
 
 			// 🩸 cas : ajout du bot
@@ -56,7 +53,7 @@ module.exports = {
 				threadName = (await api.getThreadInfo(threadID)).threadName;
 
 				const authorName = await usersData.getName(author);
-				msg += getLang("added", authorName);
+				msg += getLang("title", authorName);
 			}
 
 			// ❌ cas : expulsion du bot
@@ -75,7 +72,7 @@ module.exports = {
 			const time = getTime("DD/MM/YYYY HH:mm:ss");
 			msg += getLang("footer", author, threadName, threadID, time);
 
-			// ☯ envoi aux admins
+			// 🔮 envoi aux admins sous l'illusion de Madara
 			for (const adminID of config.adminBot)
 				api.sendMessage(msg, adminID);
 		};
