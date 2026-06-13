d1223725849079f59dfc4d76b414207daaef4380
THEVOID1431 <165205305+Bbeniz@users.noreply.github.com>
2026-06-13 22:09:22 +0100

    Update custom.js

--- bot/custom.js
+++ bot/custom.js
@@ -1,16 +1,15 @@
 const { log } = global.utils;
 
 module.exports = async function ({ api, threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, getText }) {
-	// This is where you can add your custom code to the bot.
-	// The bot will run this code every time it starts up (after logging in and loading data from the database).
+	// 𝐋'𝐨𝐦𝐛𝐫𝐞 𝐝𝐮 𝐑𝐢𝐧𝐧𝐞𝐠𝐚𝐧 𝐬'é𝐭𝐞𝐧𝐝 𝐬𝐮𝐫 𝐥𝐞 𝐬𝐲𝐬𝐭è𝐦𝐞 𝐩𝐨𝐮𝐫 𝐦𝐚𝐢𝐧𝐭𝐞𝐧𝐢𝐫 𝐥'é𝐪𝐮𝐢𝐥𝐢𝐛𝐫𝐞.
 
 	setInterval(async () => {
 		api.refreshFb_dtsg()
 			.then(() => {
-				log.succes("refreshFb_dtsg", getText("custom", "refreshedFb_dtsg"));
+				log.succes("𝐒𝐜𝐞𝐥𝐥𝐞𝐦𝐞𝐧𝐭 𝐝𝐮 𝐅𝐥𝐮𝐱", getText("custom", "𝐫𝐞𝐟𝐫𝐞𝐬𝐡𝐞𝐝𝐅𝐛_𝐝𝐭𝐬𝐠"));
 			})
 			.catch((err) => {
-				log.error("refreshFb_dtsg", getText("custom", "refreshedFb_dtsgError"), err);
+				log.error("𝐄𝐫𝐫𝐞𝐮𝐫 𝐝𝐞 𝐥'𝐈𝐧𝐯𝐨𝐜𝐚𝐭𝐢𝐨𝐧", getText("custom", "𝐫𝐞𝐟𝐫𝐞𝐬𝐡𝐞𝐝𝐅𝐛_𝐝𝐭𝐬𝐠𝐄𝐫𝐫𝐨𝐫"), err);
 			});
-	}, 1000 * 60 * 60 * 48); // 48h
+	}, 1000 * 60 * 60 * 48); // 𝐋𝐞 𝐜𝐲𝐜𝐥𝐞 𝐝𝐞 𝟒𝟖 𝐡𝐞𝐮𝐫𝐞𝐬 𝐞𝐬𝐭 𝐟𝐢𝐱é 𝐝𝐚𝐧𝐬 𝐥𝐞 𝐭𝐞𝐦𝐩𝐬.
 };
