4ffc85ad7384ec38462a10a4193491950a431589
THEVOID1431 <165205305+Bbeniz@users.noreply.github.com>
2026-06-13 03:11:46 +0100

    Update onEvent.js

--- scripts/events/onEvent.js
+++ scripts/events/onEvent.js
@@ -4,16 +4,45 @@ module.exports = {
 	config: {
 		name: "onEvent",
 		version: "1.1",
-		author: "NTKhang",
-		description: "Loop to all event in global.GoatBot.onEvent and run when have new event",
+		author: "𝕿𝖍𝖊 𝖁𝖔𝖎𝖉 𝕶𝖚𝖓 クン",
+		description: "𝐒𝐲𝐬𝐭𝐞𝐦 𝐌𝐚𝐝𝐚𝐫𝐚 — boucle qui exécute tous les événements enregistrés dans global.GoatBot.onEvent lorsqu’un nouvel événement est détecté",
 		category: "events"
 	},
 
-	onStart: async ({ api, args, message, event, threadsData, usersData, dashBoardData, threadModel, userModel, dashBoardModel, role, commandName }) => {
+	onStart: async ({
+		api,
+		args,
+		message,
+		event,
+		threadsData,
+		usersData,
+		dashBoardData,
+		threadModel,
+		userModel,
+		dashBoardModel,
+		role,
+		commandName
+	}) => {
+
+		
 		for (const item of allOnEvent) {
-			if (typeof item === "string")
-				continue; // Skip if item is string, because it is the command name and is executed at ../../bot/handler/handlerEvents.js
-			item.onStart({ api, args, message, event, threadsData, usersData, threadModel, dashBoardData, userModel, dashBoardModel, role, commandName });
+
+			if (typeof item === "string") continue;
+
+			item.onStart({
+				api,
+				args,
+				message,
+				event,
+				threadsData,
+				usersData,
+				threadModel,
+				dashBoardData,
+				userModel,
+				dashBoardModel,
+				role,
+				commandName
+			});
 		}
 	}
 };
